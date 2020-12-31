93f7c9e399d58cea011fa22fa8a38586d6f29591
Madhan Kumar <53962076+maddyexplore@users.noreply.github.com>
2020-12-31 13:07:18 +0530

    Add files via upload

---
+++ Manual_testing.py
@@ -0,0 +1,11 @@
+import crd as data_store #importing crd file as data_store
+
+# syntax
+#data_store.create(key,value,time_to_live=0) Key = Key, Value = Value , time_to_live (optional) --> create the key and value pair
+#data_store.read(key) Key = Key --> read the key and value pair
+#data_store.delete(key) Key = Key --> delete the key
+
+
+if __name__ == '__main__':
+    # write the testing here
+    None
