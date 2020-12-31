3d9a9b0f85cc0f975341b599518b4d158d34b464
Madhan Kumar <53962076+maddyexplore@users.noreply.github.com>
2020-12-31 19:57:10 +0530

    Add files via upload

--- crd.py
+++ crd.py
@@ -2,9 +2,9 @@ import threading
 import time
 import json
 import os
+
 data = {} # dictionary acts as a JSON in python
 lock = threading.Lock() # used to lock the data_file to prevent incorrect data accessing (Thread Safe)
-
 
 def check_file(): # to check json file is present or not , if not create a new one with initial data
     global data
@@ -24,29 +24,32 @@ def check_file(): # to check json file is present or not , if not create a new o
     except Exception:
         return("File is Empty")
 
-def check_size(value): # to check the size of the file and check the value size
+def check_size(key,value): # to check the size of the file and check the value size
     value = len(str(value))
-    if os.path.getsize("data_file.json")<(1024*1024*1024) and value<(16*1024*1024):
-        return True
-    elif os.path.getsize("data_file.json")>=(1024*1024*1024) and value<(16*1024*1024):
-        raise Exception("Error!! File size limit Exceeded...")
-    elif os.path.getsize("data_file.json")<(1024*1024*1024) and value>=(16*1024*1024):
-        raise Exception("Error!! Value size limit Exceeded...")
+    if os.path.getsize("data_file.json") < (1024*1024*1024):
+        if value < (16*1024*1024):
+            if len(key) <= 32:
+                return True
+            else:
+                raise Exception("Error || length of key must be capped with 32 chars")
+        else:
+            raise Exception("Error!! File size limit Exceeded...")
     else:
         raise Exception("Error!! file and value limit Exceeded...")
 
 def update():
     with open("data_file.json","w") as data_file:
         lock.acquire() # to use the data file for single thread at a time
+        # data will set as lock for updating it
         temp_data = json.dumps(data,indent=2)
         data_file.write(temp_data)
         data_file.close()
+        # data will release after updating
         lock.release()
-
 
 def create(key,value,time_to_live=0):
     try:
-        if check_size(value) and key.isalpha(): # constraints to check the size and key whether it is a alphabet or not
+        if check_size(key,value) and key.isalpha(): # constraints to check the size and key whether it is a alphabet or not
             if key not in data: # check whether key is present in data or not
                 ex_time = time.time() + time_to_live if time_to_live != 0 else 0
                 data[key] = [value,ex_time]
